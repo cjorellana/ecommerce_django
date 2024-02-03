379f9421b00c7319cb795a628bd4445f8ce1b98e
CARLOS JUAN ORELLANA <cjorellana@gmail.com>
2024-02-02 23:34:18 -0600

    se agrega un modelo

--- web/models.py
+++ web/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=200)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
+    
+
