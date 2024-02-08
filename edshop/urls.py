7c2bb0e616cbf4aa84024add1c9ba0d06a9244f8
CARLOS JUAN ORELLANA <cjorellana@gmail.com>
2024-02-07 22:21:50 -0600

    se agrega el index

--- edshop/urls.py
+++ edshop/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('web.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
 from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
+from . import views
+
+app_name = 'web'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', views.index,name='index'),
+]
