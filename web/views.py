75d260c41b5da9b6dede513a8039f91417e0efbb
CARLOS JUAN ORELLANA <cjorellana@gmail.com>
2024-02-07 22:46:57 -0600

    todo bien

--- web/views.py
+++ web/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+from .models import Categoria,Producto
+
+
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    categorias = Categoria.objects.all()
+    productos = Producto.objects.all()
+    context = {
+        'categorias':categorias,
+        'productos':productos
+    }
+    return render(request, 'index.html', context)
+    
